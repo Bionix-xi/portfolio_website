25c86eaf2c52fc5e2634bc54ac2af8b5ed6ff10c
lderuijter <85785995+lderuijter@users.noreply.github.com>
2022-03-11 14:03:18 +0100

    Add files via upload

---
+++ studiemonitor.php
@@ -0,0 +1,94 @@
+<div class="tabel">
+<table>
+  <tr class="header">
+    <th>Periode</th>
+    <th>Programmeren</th>
+    <th>Front End</th>
+    <th>Loopbaansturing</th>
+    <th>Database</th>
+    <th>Software Engeneering</th>
+    <th>Scrum</th>
+  </tr>
+  <tr>
+    <td>Sprint 1 Fase O</td>
+    <td>7.63</td>
+    <td>Voldoende</td>
+    <td>10</td>
+    <td>Onvoldoende</td>
+    <td>7</td>
+    <td>10</td>
+  </tr>
+  <tr>
+    <td>Sprint 2 Fase O</td>
+    <td>9.38</td>
+    <td>Voldoende</td>
+    <td>10</td>
+    <td>10</td>
+    <td>8.88</td>
+    <td>8.5</td>
+  </tr>
+  <tr>
+    <td>Sprint 3 Fase O</td>
+    <td>8</td>
+    <td>Goed</td>
+    <td>9.38</td>
+    <td>8</td>
+    <td>7.88</td>
+    <td>8.75</td>
+  </tr>
+  <tr>
+    <td>Sprint 4 Fase O</td>
+    <td>10</td>
+    <td>Goed</td>
+    <td>Onvoldoende</td>
+    <td>8.67</td>
+    <td>9.25</td>
+    <td>6.5</td>
+  </tr>
+  <tr>
+    <td>Sprint 5 Fase A</td>
+    <td>10</td>
+    <td>Goed</td>
+    <td>8.75</td>
+    <td>8</td>
+    <td>8.38</td>
+    <td>8.5</td>
+  </tr>
+  <tr>
+    <td>Sprint 6 Fase A</td>
+    <td>10</td>
+    <td>Voldoende</td>
+    <td>8.5</td>
+    <td>7.83</td>
+    <td>9.25</td>
+    <td>8</td>
+  </tr>
+  <tr>
+    <td>Sprint 7 Fase A</td>
+    <td>9.25</td>
+    <td>Voldoende</td>
+    <td>7.5</td>
+    <td>6.33</td>
+    <td>7.5</td>
+    <td>7</td>
+  </tr>
+  <tr>
+    <td>Sprint 8 Fase A</td>
+    <td>7.5</td>
+    <td>Goed</td>
+    <td>6</td>
+    <td>Onvoldoende</td>
+    <td>7.5</td>
+    <td>7.5</td>
+  </tr>
+  <tr>
+    <td>Sprint 9 Fase A</td>
+    <td>8</td>
+    <td>Voldoende</td>
+    <td>7</td>
+    <td>Onvoldoende</td>
+    <td>7</td>
+    <td>6</td>
+  </tr>
+</table>
+</div>
