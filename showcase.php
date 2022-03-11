c3f56b66910460656b75db8ce456e727073daa8a
lderuijter <85785995+lderuijter@users.noreply.github.com>
2022-03-11 14:09:12 +0100

    Add files via upload

--- showcase.php
+++ showcase.php
@@ -1,1 +1,11 @@
-<a href="https://github.com/lderuijter" target="_blank">Bekijk hier mijn github!</a>
+<!DOCTYPE html>
+<html>
+<body>
+    <div class="linkcontainer">
+    <p id="list">Klik hieronder voor mijn github!</p>
+    <a href="https://github.com/lderuijter" target="_blank">
+        <img id="showcaseCV" src="Images/github.png" alt="github logo">
+    </a>
+    </div>
+</body>
+</html>
